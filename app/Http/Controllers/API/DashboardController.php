bcead84913ac5e5167c5d7e15d26cc138232bbd7
Fariz Hermawan <fariz.hermawan@traveloka.com>
2019-08-06 23:25:07 +0700

    Add car ritation on dashboard

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Counter;
 use App\Driver;
 use App\Enums\Entity;
 use App\Enums\HttpStatus;
 use App\FinancialRecord;
 use App\Http\Controllers\Controller;
 use App\Transaction;
+use App\Vehicle;
 use Carbon\Carbon;
+use DB;
 
 class DashboardController extends Controller
 {
@@ -16,11 +19,21 @@ class DashboardController extends Controller
     {
         $now = Carbon::now();
 
+        $cars = Vehicle::whereFlagActive("Y")->get(["police_number"])->pluck("police_number");
+        $carStat = [];
+        foreach ($cars as $car) {
+            $counter = Counter::whereType("vehicles")
+                ->whereField($car)
+                ->whereBetween("date", [$now->startOfMonth()->toDateString(), $now->endOfMonth()->toDateString()]);
+            $carStat[$car] = $counter->sum("value");
+        }
+
         $dashboard = [
             'today_transaction' => Transaction::whereDate('created_at', '>=', $now->toDateString())->whereDate('created_at', '<=', $now->toDateString())->count(),
             'ballance' => [
                 number_format(FinancialRecord::getBallance(Entity::HO), 0, ',', '.'),
-            ]
+            ],
+            'car_stat' => $carStat
         ];
 
         return response()->json(['data' => $dashboard], HttpStatus::SUCCESS);
