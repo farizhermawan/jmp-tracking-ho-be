1ad6bfd3e7b6665e96ba63aa10ff1694975ad213
Fariz Hermawan <fariz.hermawan@traveloka.com>
2019-08-13 08:06:05 +0700

    re indent

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -13,27 +13,29 @@ use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
-    public function getInfo()
-    {
-        $now = Carbon::now();
+  public function getInfo()
+  {
+    setlocale(LC_TIME, 'Indonesian');
+    Carbon::setLocale("id");
+    $now = Carbon::now();
 
-        $cars = Vehicle::whereFlagActive("Y")->get(["police_number"])->pluck("police_number");
-        $carStat = [];
-        foreach ($cars as $car) {
-            $counter = Counter::whereType("vehicles")
-                ->whereField($car)
-                ->whereBetween("date", [$now->startOfMonth()->toDateString(), $now->endOfMonth()->toDateString()]);
-            $carStat[$car] = $counter->sum("value");
-        }
-
-        $dashboard = [
-            'today_transaction' => Transaction::whereDate('created_at', '>=', $now->toDateString())->whereDate('created_at', '<=', $now->toDateString())->count(),
-            'ballance' => [
-                number_format(FinancialRecord::getBallance(Entity::HO), 0, ',', '.'),
-            ],
-            'car_stat' => $carStat
-        ];
-
-        return response()->json(['data' => $dashboard], HttpStatus::SUCCESS);
+    $cars = Vehicle::whereFlagActive("Y")->get(["police_number"])->pluck("police_number");
+    $carStat = [];
+    foreach ($cars as $car) {
+      $counter = Counter::whereType("vehicles")
+        ->whereField($car)
+        ->whereBetween("date", [$now->startOfMonth()->toDateString(), $now->endOfMonth()->toDateString()]);
+      $carStat[$car] = $counter->sum("value");
     }
+
+    $dashboard = [
+      'today_transaction' => Transaction::whereDate('created_at', '>=', $now->toDateString())->whereDate('created_at', '<=', $now->toDateString())->count(),
+      'ballance' => [
+        number_format(FinancialRecord::getBallance(Entity::HO), 0, ',', '.'),
+      ],
+      'car_stat' => $carStat
+    ];
+
+    return response()->json(['data' => $dashboard], HttpStatus::SUCCESS);
+  }
 }
