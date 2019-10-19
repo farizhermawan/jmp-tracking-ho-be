b7f957a2d251385b6c4a6fb6d0edbc85ef55b66d
Muhammad Taufiq hardiyanto Umar <taufiqophie@gmail.com>
2019-10-20 00:23:14 +0700

    Perbaikan class: menghapus static modifier

--- app/Helpers/ReportDataGrabber.php
+++ app/Helpers/ReportDataGrabber.php
@@ -3,75 +3,75 @@ namespace App\Helpers;
 
 class ReportDataGrabber
 {
-    static private $curl;
-    static private $status;
+    const HO_URL    = 'https://trucking-ho.jmp-logistic.co.id/service/api/vehicle-cost';
+    const CDP_URL   = 'https://cdp-kontrak.jmp-logistic.co.id/service/api/undirect';
+    const KLARI_URL = 'https://klari.jmp-logistic.co.id/service/api/undirect';
+
+    private $curl;
+    private $status;
 
     function __construct($headers) {
         
-        self::$curl = curl_init();
-        curl_setopt(self::$curl, CURLOPT_POST, true);
-        curl_setopt(self::$curl, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt(self::$curl, CURLOPT_RETURNTRANSFER, true);
+        $this->curl = curl_init();
+        curl_setopt($this->curl, CURLOPT_POST, true);
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
         
-        return self::$curl;
+        return $this->curl;
     }
 
-    private static function runCurl($url, $payload)
+    private function runCurl($url, $payload)
     {
-        curl_setopt(self::$curl, CURLOPT_URL, $url);
-        curl_setopt(self::$curl, CURLOPT_POSTFIELDS, $payload);
-        $msg = curl_exec(self::$curl);
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $payload);
+        $msg = curl_exec($this->curl);
         $resp = json_decode($msg);
         
-        if($resp) self::$status = $resp->message == 'success';
+        if($resp) $this->status = $resp->message == 'success';
         return $resp;
     }
 
-    public static function getDataFromHO($data, $category)
+    public function getDataFromHO($data, $category)
     {
-        $url = 'https://trucking-ho.jmp-logistic.co.id/service/api/vehicle-cost';
-        self::$status = false;
+        $this->status = false;
         
         $data->dateStart = $data->dateStart.'T17:00:00.000Z';
         $data->dateEnd = $data->dateEnd.'T17:00:00.000Z';
         $data->category = $category ?? 'Semua';
         $payload = json_encode($data);
 
-        $responses = self::runCurl($url, $payload);
-        if(self::$status) return $responses->data;
+        $responses = self::runCurl(self::HO_URL, $payload);
+        if($this->status) return $responses->data;
         
         $data = array();
         return $data;
     }
     
-    public static function getDataFromKlari($data, $entity = ["id" => 1,"name" =>"Saldo JMP"], $kategori = 'K')
-    {
-        $url = 'https://klari.jmp-logistic.co.id/service/api/undirect';
-        
-        self::$status = false;
+    public function getDataFromKlari($data, $entity = ["id" => 1,"name" =>"Saldo JMP"], $kategori = 'K')
+    {        
+        $this->status = false;
         
         $data->category = $kategori;
         $data->entity = (object) $entity;
         $payload = json_encode($data);
         
-        $responses = self::runCurl($url, $payload);
-        if(self::$status) return $responses->data;
+        $responses = self::runCurl(self::KLARI_URL, $payload);
+        if($this->status) return $responses->data;
         
         $data = array();
         return $data;
     }
     
-    public static function getDataFromCdp($data, $group = 'K')
+    public function getDataFromCdp($data, $group = 'K')
     {
-        $url = 'https://cdp-kontrak.jmp-logistic.co.id/service/api/undirect';
         
-        self::$status = false;
+        $this->status = false;
         
         $data->group = $group;
         $payload = json_encode($data);
         
-        $responses = self::runCurl($url, $payload);
-        if(self::$status) return $responses->data;
+        $responses = self::runCurl(self::CDP_URL, $payload);
+        if($this->status) return $responses->data;
 
         $data = array();
         return $data;
