c4108ea1970895cf2ea10cd001ea9e26b54f9544
Fariz Hermawan <fariz.hermawan@traveloka.com>
2020-03-01 13:57:05 +0700

    fix auth

--- app/Http/Middleware/AuthenticateWithBasicAuth.php
+++ app/Http/Middleware/AuthenticateWithBasicAuth.php
@@ -15,7 +15,7 @@ class AuthenticateWithBasicAuth
   {
     if ($request->getUser() == null) {
       return abort(401, 'Unauthorized', ['WWW-Authenticate' => 'Basic']);
-    } else if ($request->getUser() == env('API_USERNAME') || $request->getPassword() == env('API_PASSWORD')) {
+    } else if ($request->getUser() == env('API_USERNAME') && $request->getPassword() == env('API_PASSWORD')) {
       return $next($request);
     }
     return abort(401, 'Unauthorized', ['WWW-Authenticate' => 'Basic']);
