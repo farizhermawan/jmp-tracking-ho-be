bdde5d4d7eafc413eed7a3cc044353aa2b93f161
Fariz Hermawan <fariz.hermawan@traveloka.com>
2021-05-31 10:23:25 +0700

    fix update itruck

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -176,7 +176,7 @@ class TransactionController extends Controller
     $jot = Transaction::whereId($param->key)->first();
     if (!$jot) return response()->json(['message' => 'Data tidak ditemukan!'], HttpStatus::SUCCESS);
     if ($param->field == "itruck") {
-      $jot->itruck = $param->itruck;
+      $jot->itruck = $param->value;
     }
     else if ($param->field == "container_no") {
       $jot->container_no = $param->value;
